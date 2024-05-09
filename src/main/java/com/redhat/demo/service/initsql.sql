e1b967424e2ba8b083b4e00b083058c8c3b179be
Sumit Mukherjee <69989028+summu1985@users.noreply.github.com>
2024-05-09 23:32:44 +0530

    Delete src/main/java/com/redhat/demo/service/initsql.sql

--- src/main/java/com/redhat/demo/service/initsql.sql
+++
@@ -1,9 +0,0 @@
-create database db_example;
-
-create USER 'debezium'@'%' IDENTIFIED WITH mysql_native_password BY 'debezium'; 
-
-GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium';
-
-GRANT ALL PRIVILEGES ON db_example.* TO 'debezium'@'%';
-
-FLUSH PRIVILEGES;
